f292cf4024b24513b970a444d894d834e0ef403b
daimyo-rb <daimyo@roshibros.com>
2025-05-27 11:42:26 -0500

    Send api request. Allow user to submit location. Render location data on front end

--- src/api.js
+++ src/api.js
@@ -1,2 +1,35 @@
-const API_KEY = '';
+const API_KEY = 'JP2SQV2H3VZKF3Y2ZXNRJQZM9';
+
+function buildApiStr(location) {
+  let apiKey = API_KEY;
+  const baseStr = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+  const apiStr = `${baseStr}${location}?key=${apiKey}`;
+  return apiStr;
+}
+
+async function getWeather(location) {
+  let apiStr = buildApiStr(location);
+  const res = await fetch(apiStr, {mode: 'cors'});
+  const resJson = await res.json();
+  // console.log(resJson);
+  return resJson;
+}
+function processWeatherJson(weatherJson) {
+  // want either currentConditions or days
+  let curWeather = weatherJson.currentConditions;
+  return curWeather;
+  // console.log(curWeather);
+}
+
+export async function getWeatherData(location) {
+  try {
+    let weatherJson = await getWeather(location);
+    let weatherData = processWeatherJson(weatherJson);
+    return weatherData;
+  } catch (e) {
+    console.log(`Error: ${e}`); 
+  }
+}
+
 console.log('api.js loaded');
+// getWeatherData('london')
--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import "./styles.css";
-import "./api.js"
+import { getWeatherData } from "./api.js"
+
+const formLocationInput = document.getElementById('location');
+const form = document.getElementById('weather-form');
+// const form = document.getElementById('location-btn');
+
+function populateWeatherInfo(locationData) {
+  document.querySelector('.temp > :nth-child(2)').innerText = locationData.temp;
+  document.querySelector('.conditions > :nth-child(2)').innerText = locationData.conditions;
+  document.querySelector('.precip > :nth-child(2)').innerText = locationData.precip;
+  document.querySelector('.wind > :nth-child(2)').innerText = locationData.windspeed;
+  document.querySelector('.time > :nth-child(2)').innerText = locationData.datetime;
+}
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  let location = formLocationInput.value;
+  console.log(location);
+  let locationData = await getWeatherData(location);
+  console.log(locationData);
+  populateWeatherInfo(locationData);
+})
 
 console.log('hello world');
